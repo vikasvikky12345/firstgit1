ed48b14b61111a012a30d3d5a9dafaf2f82ef2f5
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-03-21 10:36:39 +0530

    added bold and list color

---
+++ images/gitpractice/dom.js
@@ -0,0 +1,18 @@
+var li = document.getElementsByTagName('li');
+for(var i = 0;i < li.length; i++){
+    li[i].style.backgroundcolor ='green';
+}
+//var element = document.getElementsByClassName('title');
+//element.innertext ='hello';
+//console.log(li);
+li.style.fontWeight = 'bold';
+
+
+//var title =document.getElementById('header-title');
+//title.innerText = "hello"
+//console.log(title)
+//title.style.borderBottom="solid 3px black";
+//var item=document.getElementsByClassName('list-group-item');
+//console.log(item);
+//console.log(item[1]);
+//item[1].style.backgroundcolor = 'yellow';
